3a535f5b35d3fdfeb8e30eceffea65ebe33d6a05
parsasi <mail@parsa.pro>
2026-02-27 15:07:10 -0800

    WIP: trying to setup updater

--- packages/desktop/vite.config.ts
+++ packages/desktop/vite.config.ts
@@ -1,10 +1,15 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
